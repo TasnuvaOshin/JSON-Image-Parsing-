8f6b76c079468d65b5deb29628b105990a2e43bd
tasnuva tabassum oshin <tasnuva.oshin12@gmail.com>
2018-02-22 03:11:52 +0600

    Create CarModel.java

---
+++ CarModel.java
@@ -0,0 +1,27 @@
+package com.example.oshin.myimagefetching;
+
+/**
+ * Created by oshin on 2/22/2018.
+ */
+
+public class CarModel {
+
+    String name;
+    String img;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+}
